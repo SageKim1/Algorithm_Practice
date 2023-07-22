33eed578e7eaab0d77fb286a0473e96cc1980d1d
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-07-22 01:28:26 -0700

    Update 121. Best Time to Buy and Sell Stock.py

--- LeetCode/Easy/121. Best Time to Buy and Sell Stock.py
+++ LeetCode/Easy/121. Best Time to Buy and Sell Stock.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    # Time complexity: O(n) | the time grows linearly
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = prices[0]
+        
+        # O(n) | iterate through the entire array of prices once
+        for price in prices:
+            # O(1) | constant time operation
+            min_price = min(min_price, price)
+            # O(1) | constant time operation
+            max_profit = max(max_profit, price - min_price)
+        
+        return max_profit
