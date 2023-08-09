0a965524d8a633e00ad7341c27fa56c1912eedc3
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-08-09 12:43:47 -0700

    Create solution2_n.py

---
+++ LeetCode/Medium/3. Longest Substring Without Repeating Characters/solution2_n.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(min(m, n))
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        mp = {}
+
+        i = 0
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+
+            ans = max(ans, j - i + 1)
+            mp[s[j]] = j + 1
+        
+        return ans
