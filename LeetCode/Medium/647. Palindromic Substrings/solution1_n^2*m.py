6bc66a4b6d05a02d27a36845436d0647ee47326b
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-08-09 19:29:23 -0700

    Create solution1_n^2*m.py

---
+++ LeetCode/Medium/647. Palindromic Substrings/solution1_n^2*m.py
@@ -0,0 +1,14 @@
+class Solution:
+    # Time Complexity: O(n^2 * m)
+    # Space Complexity: O(n^2)
+    def countSubstrings(self, s: str) -> int:
+        length = len(s)
+        palindrome = []
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                substr = s[i:j+1]
+                if substr == substr[::-1]:
+                    palindrome.append(substr)
+
+        return len(palindrome)
