926de615a63976887330b7d097c719b1cfbaf85f
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-08-14 17:50:59 -0700

    Create Constructor.py

---
+++ DS&Algorithms/Section 13. Trees/Constructor.py
@@ -0,0 +1,30 @@
+class Node:
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    #################################
+        
+
+class BinarySearchTree:
+    ## WRITE BST CONSTRUCTOR HERE ##
+    def __init__(self):
+        self.root = None
+    ################################
+
+
+
+
+my_tree = BinarySearchTree()
+
+print("Root:", my_tree.root)
+
+
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Root: None
+
+"""
