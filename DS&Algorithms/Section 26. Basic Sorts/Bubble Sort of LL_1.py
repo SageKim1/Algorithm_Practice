8f784b7023cc5b0bdc1d5d13e8e381b7c91d5367
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-01 11:20:50 -0700

    Create Bubble Sort of LL_1.py

---
+++ DS&Algorithms/Section 26. Basic Sorts/Bubble Sort of LL_1.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    # WRITE BUBBLE_SORT METHOD HERE #
+    def bubble_sort(self):
+        if self.length < 2:
+            return
+        
+        start = self.head
+        for i in range(self.length - 1):
+            next_node = start.next
+            while next_node:
+                if start.value > next_node.value:
+                    temp = start.value
+                    start.value = next_node.value
+                    next_node.value = temp
+                next_node = next_node.next
+            
+            start = start.next
+    #################################
+
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.bubble_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
