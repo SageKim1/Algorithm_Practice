e52489b11401a8c4a0c870c68228ec2a1e6c9f51
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-01 10:16:00 -0700

    Create Selection Sort.py

---
+++ DS&Algorithms/Section 26. Basic Sorts/Selection Sort.py
@@ -0,0 +1,29 @@
+## WRITE SELECTION_SORT FUNCTION HERE ##\
+# Time Complexity: O(N^2)
+# Space Complexity: O(1)
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_idx = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_idx]:
+                min_idx = j
+        if i != min_idx:
+            temp = my_list[i]
+            my_list[i] = my_list[min_idx]
+            my_list[min_idx] = temp
+    return my_list
+######################################## 
+
+
+
+
+print(selection_sort([4,2,6,5,1,3]))
+
+
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+    
+ """
