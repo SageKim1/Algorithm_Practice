51ac329d03dfaf3071ad68fc1186ca77da2cfff6
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-02 20:04:25 -0700

    Update sol2_n.py

--- AlgoExpert/Two Number Sum/sol2_n.py
+++ AlgoExpert/Two Number Sum/sol2_n.py
@@ -2,9 +2,18 @@ def twoNumberSum(array, targetSum):
     # Write your code here.
     # Time Complexity: O(N)
     # Space Complexity: O(N)
+    '''
     my_set = set()
     for num in array:
         my_set.add(num)
         if targetSum != num*2 and (targetSum-num) in my_set:
             return [num, targetSum-num]
+    return []
+    '''
+
+    my_dict = {}
+    for num in array:
+        my_dict[num] = True
+        if targetSum != num*2 and (targetSum - num) in my_dict:
+            return [num , targetSum-num]
     return []
