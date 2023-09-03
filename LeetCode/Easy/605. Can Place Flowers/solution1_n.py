8b27b1739634bf1b74bd9afaadc716f9943e6fab
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-02 18:25:38 -0700

    Create solution1_n.py

---
+++ LeetCode/Easy/605. Can Place Flowers/solution1_n.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Approach: Single Scan
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                empty_left_plot = (i == 0) or (flowerbed[i-1] == 0)
+                empty_right_plot = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
+            
+                if empty_left_plot and empty_right_plot:
+                    flowerbed[i] = 1
+                    count += 1
+        
+        return count >= n
