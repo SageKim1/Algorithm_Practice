f4800c002cd154bbecffeffda7c901603995e5d9
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-11 12:51:54 -0700

    Update sol1_n.py

--- AlgoExpert/Validate Subsequence/sol1_n.py
+++ AlgoExpert/Validate Subsequence/sol1_n.py
@@ -1,7 +1,8 @@
 def isValidSubsequence(array, sequence):
     # Write your code here.
-    # # Time: O(N) | Space: O(1)
-    for item in reversed(array): # Time: O(N) | N: length of array
+    # Time: O(N) | N: length of array
+    # Space: O(1)
+    for item in reversed(array):
         if len(sequence) == 0:
             return True
         if item == sequence[-1]:
