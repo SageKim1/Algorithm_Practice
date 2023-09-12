c6dd28a01d7f81f46b98411e763ffc3754f370e8
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-12 12:18:11 -0700

    Create HT: Two Sum.py

---
+++ DS&Algorithms/Section 15. Hash Tables/HT: Two Sum.py
@@ -0,0 +1,39 @@
+# WRITE TWO_SUM FUNCTION HERE #
+def two_sum(nums, target):
+    num_map = {}
+    for idx, num in enumerate(nums):
+        if target - num in num_map:
+            return [idx, num_map[target-num]]
+        num_map[num] = idx
+    return []
+###############################
+    
+    
+    
+    
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+print(two_sum([1, 3, 5, 7, 9], 10))  
+print ( two_sum([1, 2, 3, 4, 5], 10) )
+print ( two_sum([1, 2, 3, 4, 5], 7) )
+print ( two_sum([1, 2, 3, 4, 5], 3) )
+print ( two_sum([], 0) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
+
+
