7b20ca571be18488549158705612620bae08dd24
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-09-11 21:55:00 -0700

    Create HT: Find Duplicates_again.py

---
+++ DS&Algorithms/Section 15. Hash Tables/HT: Find Duplicates_again.py
@@ -0,0 +1,35 @@
+# WRITE FIND_DUPLICATES FUNCTION HERE #
+def find_duplicates(nums):
+    num_counts = dict()
+    for num in nums:
+        num_counts[nums] = num_counts.get(num,0) + 1
+    duplicates = [num for num, count in num_counts.items() if count > 1]
+    return duplicates
+#######################################
+
+
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
+
