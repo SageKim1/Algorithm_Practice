e9394994aaecca92be0729acc3db6636276c11d4
SageKim <83736306+SageKim1@users.noreply.github.com>
2023-10-05 19:37:02 +0900

    Create Find Closest Value in BST.py

---
+++ AlgoExpert/Binary Search Trees/Find Closest Value in BST.py
@@ -0,0 +1,24 @@
+def findClosestValueInBst(tree, target):
+    # Write your code here.
+    closet_node = None
+    min_diff = float('inf')
+    
+    while tree != None:
+        if abs(tree.value - target) < min_diff:
+            min_diff = abs(tree.value - target)
+            closest_node = tree
+            
+        if target < tree.value:
+            tree = tree.left
+        else:
+            tree = tree.right
+
+    return closest_node.value
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
